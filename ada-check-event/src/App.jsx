fd1f6737f7aa54e129e5163dffda022a55087b80
Ursula Eponon <ulaeponon@gmail.com>
2025-10-27 16:42:17 +0100

    add Route

--- ada-check-event/src/App.jsx
+++ ada-check-event/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
-function App() {
-  
+
+export default function App() {
   return (
-    <>
-      <div>
-        
-      </div>
-     
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+       </Routes>
+  );
 }
-
