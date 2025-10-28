78e56bf43f2cb7885d52a41b340b1c88b800b00b
Ursula Eponon <ulaeponon@gmail.com>
2025-10-28 17:24:59 +0100

    Navbar update

--- ada-check-event/src/components/Navbar.jsx
+++ ada-check-event/src/components/Navbar.jsx
@@ -1,18 +1,49 @@
 import { Link } from 'react-router-dom';
-import { Home,  Search, Layers2, Heart} from 'lucide-react';
+import { Home,  Search, Layers2, Heart , X} from 'lucide-react';
+import { useState } from 'react';
 export default function Navbar() {
+  const [searchOpen, setSearchOpen]= useState(false);
+  const [categoriesOpen, setCategoriesOpen]= useState(false);
   return (
+    <>
      <nav className="navbar">
       <Link to="/"><Home size={24} />
       <span className="text">Accueil</span>
       </Link>
-      <Link to="/Search"><Search size={24} />
-      <span className="text">Recherche</span>
-      </Link>
-      <Link to="/Categories"><Layers2 size={24} />
-      <span className="text">Accueil</span></Link>
+      
+<div>
+  {!searchOpen ? (
+    <button onClick={() => setSearchOpen(true)}>
+      <Search size={24} />
+      <span>Recherche</span>
+    </button>
+  ) : (
+    <div>
+      <Search size={18} />
+      <input type="text" placeholder="Rechercher un événement..." />
+      <button onClick={() => setSearchOpen(false)}>
+        <X size={18} />
+      </button>
+    </div>
+  )}
+</div>
+
+<div>
+  <button  onClick={()=> setCategoriesOpen(!categoriesOpen)}><Layers2 size={24} />
+  <span className="text">Categories</span></button>
+  {categoriesOpen &&(
+    <ul>
+      <li><Link to="/Categories/culture">Culture & Spectacles</Link></li>
+      <li><Link to="/Categories/immersif">Jeux & Expériences immersives</Link></li>
+      <li><Link to="/Categories/sport">Sport & Bien-être</Link></li>
+      <li><Link to="/Categories/loisirs">Famille & Loisirs</Link></li>
+    </ul>
+  )}
+</div>
+  
       <Link to="/Saved"><Heart size={24} />
-      <span className="text">Accueil</span></Link>
+      <span className="text">favoris</span></Link>
     </nav>
+    </>
   );
 }
